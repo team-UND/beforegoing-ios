c478e05bb6188461879926c08d2b8dcba423b86e
heoseungjun <gjtmdwns0815@naver.com>
2025-06-18 22:39:16 +0900

    🔥Remove HTTPMethodType enum

--- BeforeGoing/Network/APIManager.swift
+++ BeforeGoing/Network/APIManager.swift
@@ -7,13 +7,13 @@ final class APIManager {
     
     func request<T: Decodable>(
         url: String,
-        method: HTTPMethodType,
+        method: HTTPMethod,
         parameters: [String: Any]? = nil,
         headers: HTTPHeaders? = nil,
         responseType: T.Type,
         completion: @escaping (Result<T, Error>) -> Void
     ) {
-        AF.request(url, method: method.type, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+        AF.request(url, method: method, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
             .validate()
             .responseDecodable(of: responseType.self) { response in
                 switch response.result {
