b80a33dda77385ddc3976ae96596abb9dc6a2bd0
heoseungjun <gjtmdwns0815@naver.com>
2025-07-24 21:35:02 +0900

    style: #15 내비게이션바 컴포넌트 제작

--- BeforeGoing/Presentation/Common/BeforeGoingNavigationBar.swift
+++ BeforeGoing/Presentation/Common/BeforeGoingNavigationBar.swift
@@ -5,3 +5,36 @@
 //  Created by APPLE on 7/22/25.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+enum NavigationBarType: Equatable {
+    
+    case clear, none
+}
+
+struct BeforeGoingNavigationBar {
+    
+    static func makeNavigationBar(
+        navigationController: UINavigationController?,
+        type: NavigationBarType
+    ) {
+        if type == .none {
+            navigationController?.navigationBar.isHidden = true
+            return
+        }
+        
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.do {
+            $0.configureWithTransparentBackground()
+            $0.shadowColor = .clear
+        }
+        
+        navigationController?.navigationBar.do {
+            $0.standardAppearance = barAppearance
+            $0.scrollEdgeAppearance = barAppearance
+        }
+    }
+}
---
+++ BeforeGoing/Presentation/Common/BeforeGoingNavigationView.swift
@@ -0,0 +1,60 @@
+//
+//  BeforeGoingNavigationView.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 7/24/25.
+//
+
+import UIKit
+
+final class BeforeGoingNavigationView: BaseView {
+        
+    private let containerView = UIView()
+    let backButton = UIButton()
+    private let titleLabel = UILabel()
+    
+    init(title: String) {
+        self.titleLabel.text = title
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setStyle() {
+        backButton.do {
+            $0.setImage(.chevronLeft, for: .normal)
+            $0.tintColor = .black
+        }
+        titleLabel.do {
+            $0.font = .custom(.headingH3)
+            $0.textColor = .black
+            $0.textAlignment = .left
+            $0.numberOfLines = 0
+        }
+    }
+    
+    override func setUI() {
+        addSubview(containerView)
+        containerView.addSubviews(
+            backButton,
+            titleLabel
+        )
+    }
+    
+    override func setLayout() {
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top)
+            $0.width.equalTo(375.adjustedW)
+        }
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(containerView.snp.top)
+            $0.width.height.equalTo(48.adjustedW)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backButton.snp.top).offset(10.adjustedH)
+            $0.leading.equalTo(backButton.snp.trailing).offset(3.adjustedW)
+        }
+    }
+}
