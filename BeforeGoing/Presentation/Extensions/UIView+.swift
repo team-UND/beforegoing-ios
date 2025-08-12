c41acda8c2410856108fd42ee7b9e3bb6decd77c
heoseungjun <gjtmdwns0815@naver.com>
2025-08-12 22:21:06 +0900

    feat: #25 특정 부분만 corenerRadius 적용되도록 UIView 확장 구현

--- BeforeGoing/Presentation/Extensions/UIView+.swift
+++ BeforeGoing/Presentation/Extensions/UIView+.swift
@@ -18,4 +18,15 @@ extension UIView {
         }
         return nil
     }
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(
+            roundedRect: self.bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
 }
