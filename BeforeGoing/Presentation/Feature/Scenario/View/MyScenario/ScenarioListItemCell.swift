69071f2303bdeeee2206542fe6d0148ab64a7255
heoseungjun <gjtmdwns0815@naver.com>
2025-09-03 14:15:16 +0900

    feat: #27 MY 시나리오 뷰 구현

--- BeforeGoing/Presentation/Feature/Scenario/View/MyScenario/ScenarioListItemCell.swift
+++ BeforeGoing/Presentation/Feature/Scenario/View/MyScenario/ScenarioListItemCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ScenarioListItemCell: UITableViewCell {
     
-    private let scenarioItemView = ScenarioItemView(height: 76)
+    private let scenarioItemView = ScenarioItemView(height: 76, isExistSubtitle: true)
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
