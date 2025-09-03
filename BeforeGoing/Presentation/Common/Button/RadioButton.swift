7972b06673e7851bad0ff25bf2d7b2fcc0ebb53f
heoseungjun <gjtmdwns0815@naver.com>
2025-09-03 22:37:51 +0900

    feat: #27 라디오 버튼 컴포넌트 구현

---
+++ BeforeGoing/Presentation/Common/Button/RadioButton.swift
@@ -0,0 +1,47 @@
+//
+//  RadioButton.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 9/3/25.
+//
+
+import UIKit
+
+final class RadioButton: UIButton {
+    
+    var currentState: RadioButtonState {
+        didSet { self.setImage(currentState.image, for: .normal) }
+    }
+    
+    init(state: RadioButtonState = .disable) {
+        self.currentState = state
+        super.init(frame: .zero)
+        setStyle()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStyle() {
+        self.setImage(currentState.image, for: .normal)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.size.equalTo(16.adjustedW)
+        }
+    }
+}
+
+extension RadioButton {
+    
+    func updateState() {
+        currentState.toggle()
+    }
+    
+    func matchState() -> Bool {
+        return currentState.matchState()
+    }
+}
---
+++ BeforeGoing/Presentation/Common/Button/RadioButtonState.swift
@@ -0,0 +1,33 @@
+//
+//  RadioButtonState.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 9/3/25.
+//
+
+import UIKit
+
+enum RadioButtonState {
+    case enable, disable
+    
+    var image: UIImage {
+        switch self {
+        case .enable:
+            return .enableRadio
+        case .disable:
+            return .disableRadio
+        }
+    }
+    
+    mutating func toggle() {
+        if self == .enable {
+            self = .disable
+            return
+        }
+        self = .enable
+    }
+    
+    func matchState() -> Bool {
+        return self == .enable ? true : false
+    }
+}
