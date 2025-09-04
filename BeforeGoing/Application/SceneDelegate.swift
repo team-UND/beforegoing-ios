c1f02d3c4c3a0fd14580a1c4ea1558152bdb137c
heoseungjun <gjtmdwns0815@naver.com>
2025-09-04 19:25:06 +0900

    chore: #27 SceneDelegate 오타 수정

--- BeforeGoing/Application/SceneDelegate.swift
+++ BeforeGoing/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let viewController = BottomNavigationViewController()
-\
+        
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = viewController
         window.makeKeyAndVisible()
