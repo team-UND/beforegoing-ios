7405d9bd44a1b6078ac97a9423b17214f36a1a87
heoseungjun <gjtmdwns0815@naver.com>
2025-09-05 18:55:20 +0900

    feat: #31 설정 메인화면 뷰컨트롤러 구현

---
+++ BeforeGoing/Presentation/Feature/Setting/ViewController/SettingViewController.swift
@@ -0,0 +1,86 @@
+//
+//  SettingViewController.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 7/24/25.
+//
+
+import UIKit
+
+final class SettingViewController: BaseViewController {
+    
+    private let rootView = SettingView()
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func setAction() {
+        rootView.accountView.seemoreView.moveButton.addTarget(
+            self,
+            action: #selector(profileButtonDidTap),
+            for: .touchUpInside
+        )
+        rootView.supportView.seemoreView.moveButton.addTarget(
+            self,
+            action: #selector(inqureButtonDidTap),
+            for: .touchUpInside
+        )
+        rootView.settingNoticeView.basicPushNoticeView.switchButton.addTarget(
+            self,
+            action: #selector(pushNoticeButtonDidTap),
+            for: .touchUpInside
+        )
+        rootView.settingNoticeView.nightPushNoticeView.switchButton.addTarget(
+            self,
+            action: #selector(nightPushNoticeButtonDidTap),
+            for: .touchUpInside
+        )
+        rootView.policyView.termView.moveButton.addTarget(
+            self,
+            action: #selector(termButtonDidTap),
+            for: .touchUpInside
+        )
+        rootView.policyView.privacyView.moveButton.addTarget(
+            self,
+            action: #selector(privacyButtonDidTap),
+            for: .touchUpInside
+        )
+    }
+}
+
+extension SettingViewController {
+    
+    @objc
+    private func profileButtonDidTap() {
+        let viewController = ProfileViewController()
+        viewController.navigationItem.hidesBackButton = true
+        viewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(viewController, animated: false)
+    }
+    
+    @objc
+    private func inqureButtonDidTap() {
+        
+    }
+    
+    @objc
+    private func pushNoticeButtonDidTap() {
+        
+    }
+    
+    @objc
+    private func nightPushNoticeButtonDidTap() {
+        
+    }
+    
+    @objc
+    private func termButtonDidTap() {
+        
+    }
+    
+    @objc
+    private func privacyButtonDidTap() {
+        
+    }
+}
