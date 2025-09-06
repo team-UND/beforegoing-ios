09d46976bcdc50a4ae5daaf01a6d7e353ee542bd
heoseungjun <gjtmdwns0815@naver.com>
2025-09-07 08:53:59 +0900

    refactor: #34 SceneDelegate 설정

--- BeforeGoing/Application/SceneDelegate.swift
+++ BeforeGoing/Application/SceneDelegate.swift
@@ -19,10 +19,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let viewController = BottomNavigationViewController()
+        let viewController = SplashViewController()
+        let navigationViewController = UINavigationController(rootViewController: viewController)
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = viewController
+        window.rootViewController = navigationViewController
         window.makeKeyAndVisible()
         self.window = window
     }
