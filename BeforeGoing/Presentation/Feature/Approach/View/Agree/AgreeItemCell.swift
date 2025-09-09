091e7134206fb6b97008da58e75262d91ec3f224
heoseungjun <gjtmdwns0815@naver.com>
2025-09-09 11:24:30 +0900

    refactor: #41 세부 약관동의 항목 터치 영역 확대

--- BeforeGoing/Presentation/Feature/Approach/View/Agree/AgreeItemCell.swift
+++ BeforeGoing/Presentation/Feature/Approach/View/Agree/AgreeItemCell.swift
@@ -77,7 +77,16 @@ final class AgreeItemCell: UITableViewCell {
     }
     
     private func setAction() {
+        setGesture()
         checkBox.addTarget(self, action: #selector(checkBoxDidTap), for: .touchUpInside)
+    }
+    
+    private func setGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(checkBoxDidTap))
+        titleLabel.do {
+            $0.isUserInteractionEnabled = true
+            $0.addGestureRecognizer(tapGesture)
+        }
     }
 }
 
