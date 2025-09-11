45bb95a90dec458eb1784c285d722c11ce30c997
heoseungjun <gjtmdwns0815@naver.com>
2025-09-11 23:10:16 +0900

    feat: #43 로그인 엔티티 설정

---
+++ BeforeGoing/Domain/Entity/LoginEntity.swift
@@ -0,0 +1,14 @@
+//
+//  LoginEntity.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 9/11/25.
+//
+
+struct LoginEntity {
+    let tokenType: String
+    let accessToken: String
+    let accessTokenExpiresIn: Int
+    let refreshToken: String
+    let refreshTokenExpiresIn: Int
+}
