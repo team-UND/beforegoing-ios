d5527daf2f25fb25968acd06a9f292824e6abda5
heoseungjun <gjtmdwns0815@naver.com>
2025-09-15 13:57:13 +0900

    refactor: #43 앱 객체 생성 시점에 의존성 주입

--- BeforeGoing/Application/AppDelegate.swift
+++ BeforeGoing/Application/AppDelegate.swift
@@ -13,6 +13,9 @@ import KakaoSDKAuth
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        DIContainer.shared.injectDependency()
+        
         if let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String {
             KakaoSDK.initSDK(appKey: appKey)
         } else {
