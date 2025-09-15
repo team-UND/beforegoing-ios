e6a0e45edb55e2c38b3370ac7e14e61bb9d59c6e
heoseungjun <gjtmdwns0815@naver.com>
2025-09-15 14:03:46 +0900

    feat: #43 카카오 로그인 버튼 클릭 이벤트 처리

--- BeforeGoing/Presentation/Feature/Approach/ViewController/LoginViewController.swift
+++ BeforeGoing/Presentation/Feature/Approach/ViewController/LoginViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 final class LoginViewController: BaseViewController {
     
     private let rootView = LoginView()
+    private let viewModel: LoginViewModel
+    
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = rootView
@@ -27,7 +37,18 @@ extension LoginViewController {
     
     @objc
     func kakaoLoginButtonDidTap() {
-        print("Kakao Did Tap")
+        Task {
+            do {
+                try await viewModel.action(input: .kakaoLoginDidTap)
+                // 이미 있는 회원이라면 홈
+                // 첫 회원가입이라면 약관동의로 이동
+                let viewController = AgreeTermsViewController(viewModel: AgreeItemViewModel())
+                viewController.navigationItem.hidesBackButton = true
+                self.navigationController?.pushViewController(viewController, animated: true)
+            } catch(let error) {
+                BeforeGoingLogger.error(error)
+            }
+        }
     }
     
     @objc
