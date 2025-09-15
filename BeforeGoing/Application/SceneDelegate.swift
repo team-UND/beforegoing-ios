485d18b817773cd4d69941f34325dfc75556636d
heoseungjun <gjtmdwns0815@naver.com>
2025-09-15 13:57:29 +0900

    refactor: #43 ViewControllerFactory를 활용하여 첫 화면 설정

--- BeforeGoing/Application/SceneDelegate.swift
+++ BeforeGoing/Application/SceneDelegate.swift
@@ -19,8 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let viewController = SplashViewController()
-        let navigationViewController = UINavigationController(rootViewController: viewController)
+        let splashViewController = ViewControllerFactory.shared.makeSplashViewController()
+        let navigationViewController = UINavigationController(rootViewController: splashViewController)
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = navigationViewController
