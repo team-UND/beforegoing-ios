cb81df6fa2d8d64d7756fa290daa1a8a2e7fee28
heoseungjun <gjtmdwns0815@naver.com>
2025-09-17 22:41:15 +0900

    refactor: #45 유스케이스가 구체 타입이 아닌 인터페이스를 주입받도록 변경

--- BeforeGoing/Domain/UseCase/AutoLoginUseCase.swift
+++ BeforeGoing/Domain/UseCase/AutoLoginUseCase.swift
@@ -7,9 +7,9 @@
 
 struct AutoLoginUseCase {
     
-    private let repository: AuthRepository
+    private let repository: AuthInterface
     
-    init(repository: AuthRepository) {
+    init(repository: AuthInterface) {
         self.repository = repository
     }
     
