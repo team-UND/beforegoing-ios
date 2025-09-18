08f7683a993c798b9c7abb4d838e25983793ac53
heoseungjun <gjtmdwns0815@naver.com>
2025-09-19 08:27:55 +0900

    chore: #45 UI 테스트 주석 제거

--- BeforeGoingUITests/BeforeGoingUITestsLaunchTests.swift
+++ BeforeGoingUITests/BeforeGoingUITestsLaunchTests.swift
@@ -5,7 +5,6 @@
 //  Created by APPLE on 5/21/25.
 //
 
-/*
 import XCTest
 
 final class BeforeGoingUITestsLaunchTests: XCTestCase {
@@ -32,4 +31,3 @@ final class BeforeGoingUITestsLaunchTests: XCTestCase {
         add(attachment)
     }
 }
-*/
