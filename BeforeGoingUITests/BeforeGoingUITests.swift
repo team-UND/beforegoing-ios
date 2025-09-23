c701005c4248c29438ee251b4b8b44bee1e452a1
heoseungjun <gjtmdwns0815@naver.com>
2025-09-23 14:08:23 +0900

    chore: #48 불필요한 테스트 로직 삭제

--- BeforeGoingUITests/BeforeGoingUITests.swift
+++ BeforeGoingUITests/BeforeGoingUITests.swift
@@ -34,9 +34,6 @@ final class BeforeGoingUITests: XCTestCase {
     
     @MainActor
     func testLaunchPerformance() throws {
-        let app = XCUIApplication()
-        app.launchEnvironment = ["USE_MOCK": "true"]
-        
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
