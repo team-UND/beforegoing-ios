98940a462958fdbc6d4ea5147d4ab707ea697e79
heoseungjun <gjtmdwns0815@naver.com>
2025-09-27 16:33:50 +0900

    refactor: #53 NotificationDTO를 프로토콜 -> 열거형으로 변경

--- BeforeGoing/Data/Model/Notification/NotificationDTO.swift
+++ BeforeGoing/Data/Model/Notification/NotificationDTO.swift
@@ -5,16 +5,19 @@
 //  Created by APPLE on 9/26/25.
 //
 
-protocol NotificationDTO: Codable {}
+enum NotificationDTO: Encodable {
+    case active(ActiveNotificationDTO)
+    case inactive(InactiveNotificationDTO)
+}
 
-struct ActiveNotificationDTO: NotificationDTO {
+struct ActiveNotificationDTO: Encodable {
     var isActive: Bool = true
     let notificationType: String
     let notificationMethodType: String
     let daysOfWeekOrdinal: [Int]
 }
 
-struct InactiveNotificationDTO: NotificationDTO {
+struct InactiveNotificationDTO: Encodable {
     var isActive: Bool = false
     let notificationType: String
 }
