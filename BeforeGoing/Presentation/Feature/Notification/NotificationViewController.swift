1c7db0f9794915b1ea818602e1c01c451ede6f29
heoseungjun <gjtmdwns0815@naver.com>
2025-11-04 16:03:49 +0900

    refactor: #79 알람 화면에 시나리오명 반영

--- BeforeGoing/Presentation/Feature/Notification/NotificationViewController.swift
+++ BeforeGoing/Presentation/Feature/Notification/NotificationViewController.swift
@@ -19,7 +19,10 @@ final class NotificationViewController: BaseViewController {
         content: UNNotificationContent,
         identifier: String
     ) {
-        self.rootView = NotificationView(notificationViewType: notificationViewType)
+        self.rootView = NotificationView(
+            notificationViewType: notificationViewType,
+            title: content.title
+        )
         self.content = content
         self.identifier = identifier
         super.init(nibName: nil, bundle: nil)
