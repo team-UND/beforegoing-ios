79a7a6cadbe1d54d2df48af0b37a414a95f778d2
heoseungjun <gjtmdwns0815@naver.com>
2025-11-23 11:56:11 +0900

    refactor: #81 앱 종료 시 알림 로직을 AppDelegate -> SceneDelegate로 이동

--- BeforeGoing/Application/AppDelegate.swift
+++ BeforeGoing/Application/AppDelegate.swift
@@ -47,9 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        Task {
-            await NotificationManager.shared.pushTerminateNotification()
-        }
+        
     }
 }
 
