669bcae7af55d3a05d4c4aa96a5ba0935b5aa8bb
heoseungjun <gjtmdwns0815@naver.com>
2025-12-12 15:13:28 +0900

    refactor: #81 5분 뒤 알림을 예약하도록 설정
    기존 : 6초

--- BeforeGoing/Presentation/Feature/Notification/NotificationViewController.swift
+++ BeforeGoing/Presentation/Feature/Notification/NotificationViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 final class NotificationViewController: BaseViewController {
     
+    private let snoozeTime: Double = 5
     private let rootView: NotificationView
     private let content: UNNotificationContent
     private let identifier: String
@@ -69,7 +70,7 @@ extension NotificationViewController {
         NotificationManager.shared.reserveSnooze(
             originalContent: self.content,
             identifier: self.identifier,
-            delayMinutes: 0.1
+            delayMinutes: snoozeTime
         )
     }
     
