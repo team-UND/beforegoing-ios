5532896880dfdcf2c87049db610289fd4019d010
heoseungjun <gjtmdwns0815@naver.com>
2025-12-29 21:41:19 +0900

    refactor: #91 로그인 만료 모달 액션에 로그인 뷰컨트롤러 이동 로직 추가

--- BeforeGoing/Data/Network/Service/TokenReissuer.swift
+++ BeforeGoing/Data/Network/Service/TokenReissuer.swift
@@ -81,7 +81,11 @@ extension TokenReissuer {
             let modalView = ModalView(type: .expirationLogin)
             let modalViewController = ModalViewController(
                 modalView: modalView,
-                action: { viewController.dismiss(animated: true) }
+                action: {
+                    viewController.dismiss(animated: true)
+                    let loginViewController = ViewControllerFactory.shared.makeLoginViewController()
+                    ViewControllerUtil.replaceRootViewController(to: loginViewController)
+                }
             )
             viewController.present(modalViewController, animated: true)
         }
