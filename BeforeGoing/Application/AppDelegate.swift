908f1418e88ee59e069fddddb70e503bf7dce9a7
heoseungjun <gjtmdwns0815@naver.com>
2026-01-12 11:37:53 +0900

    refactor: #99 카카오 SDK 초기화 로직 함수 분리

--- BeforeGoing/Application/AppDelegate.swift
+++ BeforeGoing/Application/AppDelegate.swift
@@ -16,12 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         DIContainer.shared.injectDependency()
+        initKakaoSDK()
         
-        if let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String {
-            KakaoSDK.initSDK(appKey: appKey)
-        } else {
-            fatalError("카카오 네이티브 앱 키 없음")
-        }
         return true
     }
     
@@ -49,3 +45,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {}
 }
 
+extension AppDelegate {
+    
+    func initKakaoSDK() {
+        if let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String {
+            KakaoSDK.initSDK(appKey: appKey)
+        } else {
+            fatalError("카카오 네이티브 앱 키 없음")
+        }
+    }
+}
