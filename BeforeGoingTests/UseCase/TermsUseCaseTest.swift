62f6a1cb17c116748580238400fe951a8e48560b
heoseungjun <gjtmdwns0815@naver.com>
2026-02-25 17:55:30 +0900

    test: #101 TermsStorage 기능 테스트

---
+++ BeforeGoingTests/UseCase/TermsUseCaseTest.swift
@@ -0,0 +1,74 @@
+//
+//  TermsRepositoryTest.swift
+//  BeforeGoing
+//
+//  Created by APPLE on 2/3/26.
+//
+
+import CoreData
+import Testing
+@testable import BeforeGoing
+
+struct TermsUseCaseTest {
+    
+    private let context: NSManagedObjectContext
+    private let userDefaultsService: MockuserDefaultsService
+    private let repository: TermsInterface
+    private let fetchAgreeUseCase: FetchAgreeTermsUseCase
+    private let sendAgreeTermsUseCase: SendAgreeTermsUseCase
+    private let updatePushNoticeUseCase: UpdatePushNoticeUseCase
+    
+    init() async throws {
+        self.userDefaultsService = MockuserDefaultsService()
+        self.context = ContextProvider.makeMockContext()
+        self.repository = TermsStorage(userDefaultsService: userDefaultsService, context: context)
+        self.fetchAgreeUseCase = FetchAgreeTermsUseCase(repository: repository)
+        self.sendAgreeTermsUseCase = SendAgreeTermsUseCase(repository: repository)
+        self.updatePushNoticeUseCase = UpdatePushNoticeUseCase(repository: repository)
+        
+        try await createMember(id: 1, nickname: "tester")
+    }
+    
+    @Test("약관 동의 내역 저장", arguments: [true, false])
+    func sendAgreeTerms_success(eventPushAgreed: Bool) async throws {
+        try await sendAgreeTermsUseCase.execute(
+            termsOfServiceAgreed: true,
+            privacyPolicyAgreed: true,
+            isOver14: true,
+            eventPushAgreed: eventPushAgreed
+        )
+        
+        let result = try await fetchAgreeUseCase.execute()
+        
+        #expect(result?.eventPushAgreed == eventPushAgreed)
+    }
+    
+    @Test("약관 동의 내역 수정", arguments: [true, false])
+    func fetchAgreeTerms_success(eventPushAgreed: Bool) async throws {
+        try await sendAgreeTermsUseCase.execute(
+            termsOfServiceAgreed: true,
+            privacyPolicyAgreed: true,
+            isOver14: true,
+            eventPushAgreed: eventPushAgreed
+        )
+        
+        try await updatePushNoticeUseCase.execute(eventPushAgreed: !eventPushAgreed)
+        
+        let result = try await fetchAgreeUseCase.execute()
+        
+        #expect(result?.eventPushAgreed == !eventPushAgreed)
+    }
+    
+    private func createMember(id: Int, nickname: String) async throws {
+        let _ = userDefaultsService.save(id, key: .userID)
+        
+        try await context.perform {
+            let member = Member(context: self.context)
+            member.id = Int64(id)
+            member.nickname = nickname
+            member.createdAt = .now
+            member.updatedAt = .now
+            try self.context.save()
+        }
+    }
+}
